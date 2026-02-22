6e3c1d33a36d08232c2accb63a45793e79f2d4a5
KiandroDW <kiandro.dewaegenaere@ugent.be>
2026-02-22 14:57:00 +0000

    Fixed some wrong code

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,27 +1,33 @@
 # Configuration of integration tests
 import pytest
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from fastapi.testclient import TestClient
+from database import Database
 
 from src.main import app
 from src.database import get_db
 
 TEST_DATABASE_URL = "" # TBD
 
-# Create test engine
-test_engine = create_async_engine(TEST_DATABASE_URL, echo=False)
-TestSessionLocal = async_sessionmaker(test_engine, expire_on_commit=False)
+test_database = Database(TEST_DATABASE_URL)
 
 
 # Override dependency
 async def override_get_db():
-    async with TestSessionLocal() as session:
-        yield session
+    return test_database
 
 
-@pytest.fixure(scope="session")
+# Start connectie met database
+@pytest.mark.asyncio
+@pytest.fixture(scope="session", autouse=True)
+async def connect_test_db():
+    await test_database.connect()
+    yield
+    await test_database.disconnect()
+
+
+@pytest.fixture(scope="session")
 def client():
-    app.dependecy_overrides[get_db] = override_get_db
+    app.dependency_overrides[get_db] = override_get_db
     with TestClient(app) as c:
         yield c
     app.dependency_overrides.clear()
