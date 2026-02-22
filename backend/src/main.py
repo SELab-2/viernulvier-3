071f49b9f69ea2a139cd6bc4ae4d6a3df2438c90
nathan <nathan.vermeiren@ugent.be>
2026-02-22 15:15:48 +0100

    setup main file for backend with FastAPI, and add documentation for contributers

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,2 +1,13 @@
-if __name__ == "__main__":
-    pass
+
+from fastapi import FastAPI
+
+from routers.events import router as events_router
+from routers.productions import router as production_router
+
+app = FastAPI(
+    title="VierNulVier-Archief-API",
+    version="0.1.0",
+)
+
+app.include_router(events_router)
+app.include_router(production_router)
