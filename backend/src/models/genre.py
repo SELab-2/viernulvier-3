cca9dfdcbb3e0867437c7dae23af8a786fd78525
Alexander Vandeputte <alexander.vandeputte88@gmail.com>
2026-02-23 18:17:34 +0100

    added language functionality to genres

--- backend/src/models/genre.py
+++ backend/src/models/genre.py
@@ -1,6 +1,6 @@
 """SQLAlchemy-model voor genres."""
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from src.database import Base
 from src.models.associations import prod_genres
@@ -10,8 +10,19 @@ class Genre(Base):
     __tablename__ = "genres"
 
     id = Column(Integer, primary_key=True)
-    name = Column(String)
+    names = relationship("GenreName", back_populates="genre")
 
     productions = relationship(
         "Production", secondary=prod_genres, back_populates="genres"
     )
+
+class GenreName(Base):
+    __tablename__ = "genre_names"
+    genre_id = Column(Integer, ForeignKey("genres.id"), primary_key=True)
+    language_id = Column(Integer, ForeignKey("language.id"), primary_key=True)
+
+    name = Column(String)
+
+    genre = relationship("Genre", back_populates="names")
+    language = relationship("Language")
+
