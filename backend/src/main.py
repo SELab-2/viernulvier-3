dd7b458f7c3021ec550899e381ccc142e0fe1096
nathan <nathan.vermeiren@ugent.be>
2026-02-23 11:53:02 +0100

    change the fastAPI_usage.md file so it corresponds with new file structure

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,13 +1,15 @@
 
 from fastapi import FastAPI
 
-from api.v1.archive import global_router
-
+from api.v1.archive import global_archive_router
+from api.v1.auth import global_auth_router
 
 app = FastAPI(
     title="VierNulVier-Archief-API",
     version="0.1.0",
 )
 
-app.include_router(global_router)
+app.include_router(global_archive_router)
+app.include_router(global_auth_router)
+
 
