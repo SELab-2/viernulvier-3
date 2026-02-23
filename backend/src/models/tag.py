5fb02d8c8ffdc1cb59c00e9e16ea2cb28deb4020
Alexander Vandeputte <alexander.vandeputte88@gmail.com>
2026-02-23 18:25:19 +0100

    added language functionality to tags

--- backend/src/models/tag.py
+++ backend/src/models/tag.py
@@ -1,6 +1,6 @@
 """SQLAlchemy-model voor tags."""
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from src.database import Base
 from src.models.associations import prod_tags
@@ -10,6 +10,17 @@ class Tag(Base):
     __tablename__ = "tags"
 
     id = Column(Integer, primary_key=True)
-    name = Column(String)
+    names = relationship("TagName", back_populates="tag")
 
     productions = relationship("Production", secondary=prod_tags, back_populates="tags")
+
+
+class TagName(Base):
+    __tablename__ = "tag_names"
+    tag_id = Column(Integer, ForeignKey("tags.id"), primary_key=True)
+    language_id = Column(Integer, ForeignKey("language.id"), primary_key=True)
+
+    name = Column(String)
+
+    tag = relationship("Tag", back_populates="names")
+    language = relationship("Language")
