e9741f1abe0f41415dd57bb69dbdc4cef44719f9
Mattis Vander Donckt <mattis.vanderdonckt@ugent.be>
2026-02-23 12:01:02 +0000

    Comment out CORS middleware configuration and update CORS_ORIGINS in settings

--- backend/src/main.py
+++ backend/src/main.py
@@ -15,13 +15,13 @@ app = FastAPI(
     root_path="/api",
 )
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=settings.CORS_ORIGINS,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=settings.CORS_ORIGINS,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 
 @app.get("/health")
