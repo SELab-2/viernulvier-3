d3dd7ffbe4768ceea3f59fd1a92cac6eaf3fb497
KiandroDW <kiandro.dewaegenaere@ugent.be>
2026-02-24 13:42:44 +0100

    made code compatible with PR #21

--- backend/src/database.py
+++ backend/src/database.py
@@ -1,7 +1,5 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
-
-# URL van echte database
-DATABASE_URL = "" # TBD
+from src.config import DATABASE_URL
 
 engine = create_async_engine(DATABASE_URL, echo=False)
 AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
