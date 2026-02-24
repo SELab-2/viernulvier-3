e2fc156f91a877002d0d0a08e65f91edccccf324
elerhels <elias.verhelst@ugent.be>
2026-02-24 16:26:34 +0100

    minimalize fronted boilerplate

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -1,13 +1,8 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
-
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
-}
-
 export default function Home() {
-  return <Welcome />;
+  return <div>
+    <title>Archief | VIERNULVIER</title>
+
+    <h1 className="text-3xl font-bold">Homepagina archief</h1>
+  </div>;
 }
+
