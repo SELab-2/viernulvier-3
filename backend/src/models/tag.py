ac74e2dbb2dc5a1fb4a33183f645c690ab260e55
KiandroDW <kiandro.dewaegenaere@ugent.be>
2026-03-04 14:13:10 +0100

    Apply black formatting

--- backend/src/models/tag.py
+++ backend/src/models/tag.py
@@ -10,7 +10,7 @@ class Tag(Base):
     __tablename__ = "tags"
 
     id = Column(Integer, primary_key=True)
-    
+
     names = relationship("TagName", back_populates="tag")
     productions = relationship("Production", secondary=prod_tags, back_populates="tags")
 
