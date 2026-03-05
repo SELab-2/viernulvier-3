5598b8e27a81ac5ef23e3a4bd83515f779fceaba
Mattis Vander Donckt <mattis.vanderdonckt@ugent.be>
2026-03-05 19:49:27 +0100

    Refactor authentication settings to use JWT_ prefix

--- backend/src/config.py
+++ backend/src/config.py
@@ -25,8 +25,8 @@ class Settings(BaseSettings):
     DATABASE_HOST: str
     DATABASE_PORT: int
 
-    SECRET_KEY: str
-    ALGORITHM: str = "HS256"
+    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     REFRESH_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 30  # 30 dagen
 
