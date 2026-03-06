10a4e264cce0ee5f601071d5aa1b8394ce2b3f40
Stijn Van Biesen <Stijn.VanBiesen@UGent.be>
2026-03-06 15:27:55 +0100

    Updated schema for productions.

--- backend/src/schemas/production.py
+++ backend/src/schemas/production.py
@@ -1,70 +1,78 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
 from typing import Optional
 from datetime import datetime
+   
+# class ProductionInfoUpdate(BaseModel):
+#     title: Optional[str] = None
+#     supertitle: Optional[str] = None
+#     artist: Optional[str] = None
+#     tagline: Optional[str] = None
+#     teaser: Optional[str] = None
+#     description: Optional[str] = None
+#     info: Optional[str] = None
+
+# class ProductionUpdate(BaseModel):
+#     performer_type: Optional[str] = None
+#     attendance_mode: Optional[str] = None
+#     media_gallery_id: Optional[int] = None
+#     created_at: Optional[datetime] = None
+#     updated_at: Optional[datetime] = None
 
 class Pagination(BaseModel):
-    next_cursor: int | None
-    has_more: bool
+    next_cursor: int | None = None
+    has_more: bool = False
 
+# When a new production info is created in a specific language.
 class ProductionInfoCreate(BaseModel):
     production_id: int
     language_id: int
-    title: Optional[str]
-    supertitle: Optional[str]
-    artist: Optional[str]
-    tagline: Optional[str]
-    teaser: Optional[str]
-    description: Optional[str]
-    info: Optional[str]
-
-class ProductionCreate(BaseModel):
-    performer_type: Optional[str]
-    attendance_mode: Optional[str]
-    media_gallery_id: Optional[int]
-    created_at: Optional[datetime]
-    updated_at: Optional[datetime]
-   
-class ProductionInfoUpdate(BaseModel):
     title: Optional[str] = None
     supertitle: Optional[str] = None
     artist: Optional[str] = None
     tagline: Optional[str] = None
-    teaser: Optional[str] = None
+    teaser: Optional[str] = None    
     description: Optional[str] = None
     info: Optional[str] = None
 
-class ProductionUpdate(BaseModel):
+# When a new production is created (has one or more info entries).
+class ProductionCreate(BaseModel):
     performer_type: Optional[str] = None
     attendance_mode: Optional[str] = None
     media_gallery_id: Optional[int] = None
     created_at: Optional[datetime] = None
     updated_at: Optional[datetime] = None
 
+# The response for a production info in a specific language.
 class ProductionInfoResponse(BaseModel):
-    production_id: int
-    language_id: int
-    title: Optional[str]
-    supertitle: Optional[str]
-    artist: Optional[str]
-    tagline: Optional[str]
-    teaser: Optional[str]
-    description: Optional[str]
-    info: Optional[str]
+    id_url: str
+    production_id_url: str
+    language_id_url: str
+    title: Optional[str] = None
+    supertitle: Optional[str] = None
+    artist: Optional[str] = None 
+    tagline: Optional[str] = None
+    teaser: Optional[str] = None 
+    description: Optional[str] = None
+    info: Optional[str] = None
 
     model_config = ConfigDict(from_attributes=True)
 
 class ProductionResponse(BaseModel):
-    id: str
-    performer_type: Optional[str]
-    attendance_mode: Optional[str]
-    media_gallery_id: Optional[int]
-    created_at: Optional[datetime]
-    updated_at: Optional[datetime]
-    # Get all production infos for this production (if multiple languages are supported, there will be multiple production infos).
-    info: list[ProductionInfoResponse]
+    id_url: str
+    performer_type: Optional[str] = None
+    attendance_mode: Optional[str] = None
+    media_gallery_id: Optional[int] = None
+    created_at: Optional[datetime] = None
+    updated_at: Optional[datetime] = None
+
+    # A production has a list of event urls (for the different events of that producttion).
+    # A production has a list of info urls (for different languages). 
+    info: list[ProductionInfoResponse] = Field(default_factory=list)
+    events: list[str] = Field(default_factory=list)
 
     model_config = ConfigDict(from_attributes=True)
 
+# The response for a list of productions, including pagination info.
 class ProductionListResponse(BaseModel):
-    productions: list[ProductionResponse]
-    pagination: Pagination
+    productions: list[ProductionResponse] = Field(default_factory=list)
+    pagination: Pagination = Field(default_factory=Pagination)
