95ec4490534850efb0c5b380ce46f6cb19a27bd4
Stijn Van Biesen <Stijn.VanBiesen@UGent.be>
2026-03-06 19:01:01 +0100

    Modified production service/schema.

--- backend/src/schemas/production.py
+++ backend/src/schemas/production.py
@@ -1,50 +1,20 @@
+from enum import Enum
+
 from pydantic import BaseModel, ConfigDict, Field
 from typing import Optional
 from datetime import datetime
    
-# class ProductionInfoUpdate(BaseModel):
-#     title: Optional[str] = None
-#     supertitle: Optional[str] = None
-#     artist: Optional[str] = None
-#     tagline: Optional[str] = None
-#     teaser: Optional[str] = None
-#     description: Optional[str] = None
-#     info: Optional[str] = None
-
-# class ProductionUpdate(BaseModel):
-#     performer_type: Optional[str] = None
-#     attendance_mode: Optional[str] = None
-#     media_gallery_id: Optional[int] = None
-#     created_at: Optional[datetime] = None
-#     updated_at: Optional[datetime] = None
-
 class Pagination(BaseModel):
     next_cursor: int | None = None
     has_more: bool = False
 
-# When a new production info is created in a specific language.
-class ProductionInfoCreate(BaseModel):
-    production_id: int
-    language_id: int
-    title: Optional[str] = None
-    supertitle: Optional[str] = None
-    artist: Optional[str] = None
-    tagline: Optional[str] = None
-    teaser: Optional[str] = None    
-    description: Optional[str] = None
-    info: Optional[str] = None
-
-# When a new production is created (has one or more info entries).
-class ProductionCreate(BaseModel):
-    performer_type: Optional[str] = None
-    attendance_mode: Optional[str] = None
-    media_gallery_id: Optional[int] = None
-    created_at: Optional[datetime] = None
-    updated_at: Optional[datetime] = None
+class UpdateAction(str, Enum):
+    UPSERT = "upsert"
+    DELETE = "delete"
 
 # The response for a production info in a specific language.
 class ProductionInfoResponse(BaseModel):
-    id_url: str
+    #id_url: str
     production_id_url: str
     language_id_url: str
     title: Optional[str] = None
@@ -76,3 +46,43 @@ class ProductionResponse(BaseModel):
 class ProductionListResponse(BaseModel):
     productions: list[ProductionResponse] = Field(default_factory=list)
     pagination: Pagination = Field(default_factory=Pagination)
+
+# When a new production info is created in a specific language.
+class ProductionInfoCreate(BaseModel):
+    production_id: int
+    language_id: int
+    title: Optional[str] = None
+    supertitle: Optional[str] = None
+    artist: Optional[str] = None
+    tagline: Optional[str] = None
+    teaser: Optional[str] = None    
+    description: Optional[str] = None
+    info: Optional[str] = None
+
+# When a new production is created (has one or more info entries).
+class ProductionCreate(BaseModel):
+    performer_type: Optional[str] = None
+    attendance_mode: Optional[str] = None
+    media_gallery_id: Optional[int] = None
+    created_at: Optional[datetime] = None
+    updated_at: Optional[datetime] = None
+
+class ProductionInfoUpdate(BaseModel):
+    language_id: int
+    action: UpdateAction = UpdateAction.UPSERT
+
+    title: Optional[str] = None
+    supertitle: Optional[str] = None
+    artist: Optional[str] = None
+    tagline: Optional[str] = None
+    teaser: Optional[str] = None
+    description: Optional[str] = None
+    info: Optional[str] = None
+
+class ProductionUpdate(BaseModel):
+    performer_type: Optional[str] = None
+    attendance_mode: Optional[str] = None
+    media_gallery_id: Optional[int] = None
+    created_at: Optional[datetime] = None
+    updated_at: Optional[datetime] = None
+    production_infos: Optional[list[ProductionInfoUpdate]] = None
--- backend/src/services/production.py
+++ backend/src/services/production.py
@@ -1,13 +1,17 @@
-from backend.src.schemas.production import Pagination
+from multiprocessing import Event
+
+from backend.src.schemas.production import Pagination, UpdateAction
 from sqlalchemy.orm import Session
 from src.models import Production, ProdInfo, Language
-from src.schemas import ProductionCreate, ProductionInfoCreate, ProductionResponse, ProductionInfoResponse, ProductionListResponse
+from src.schemas import ProductionCreate, ProductionInfoCreate, ProductionUpdate, ProductionResponse, ProductionInfoResponse, ProductionListResponse
 
 # The response functions: both return copies.
 def build_production_info_response(production_info: ProdInfo, base_url: str) -> ProductionInfoResponse:
     return ProductionInfoResponse(
-        production_id=f"{base_url}/productions/{production_info.production_id}",
-        language_id=f"{base_url}/languages/{production_info.language_id}",
+        #id_url=f"{base_url}/productions/{production_info.production_id}/infos/{production_info.language_id}",
+        production_id_url=f"{base_url}/productions/{production_info.production_id}",
+        language_id_url=f"{base_url}/languages/{production_info.language_id}",
+        
         title=production_info.title,
         supertitle=production_info.supertitle,
         artist=production_info.artist,
@@ -17,21 +21,34 @@ def build_production_info_response(production_info: ProdInfo, base_url: str) -> 
         info=production_info.info
     )
 
-def build_production_response(db: Session, production: Production, base_url: str) -> ProductionResponse:
-    production_infos = db.query(ProdInfo).filter(ProdInfo.production_id == production.id).all()    
-    production_infos = [build_production_info_response(db_prod_info, base_url) for db_prod_info in production_infos]
+def build_production_response(db: Session, production: Production, base_url: str, language_id: int | None) -> ProductionResponse:    
+    # If language is provided, only get that specific info (if it exists, otherwise error). Otherwise, get all infos.
+    if language_id is not None:
+        production_infos = db.query(ProdInfo).filter(ProdInfo.production_id == production.id, ProdInfo.language_id == language_id).all()
+        if not production_infos:
+            raise ValueError(f"Production info with production id '{production.id}' and language id '{language_id}' not found.")
+    else:
+        production_infos = db.query(ProdInfo).filter(ProdInfo.production_id == production.id).all()
+
+    # Convert to response models.
+    production_infos = [build_production_info_response(production_info, base_url) for production_info in production_infos]
+
+    # Get events of this production.
+    events = get_events_for_production_service(db, production.id, base_url)
 
     return ProductionResponse(
         id=f"{base_url}/productions/{production.id}",
+        
         performer_type=production.performer_type,
         attendance_mode=production.attendance_mode,
         media_gallery_id=production.media_gallery_id,
         created_at=production.created_at,
         updated_at=production.updated_at,
-        info=production_infos
+        info=production_infos,
+        events=events
     )
 
-# /GET /productions --> Use pagination to return a part of all productions.
+# Uses pagination to return a part of all productions.
 def get_productions_service(db: Session, base_url: str, cursor: int | None = None, limit: int = 20) -> ProductionListResponse: 
     query = db.query(Production).order_by(Production.id)
     if cursor is not None:
@@ -52,60 +69,33 @@ def get_productions_service(db: Session, base_url: str, cursor: int | None = Non
             has_more=has_more
         )
     )   
+
+# Returns all event-urls for a given production.   
+def get_events_for_production_service(db: Session, production_id: int, base_url: str) -> list[str]:
+    production = db.query(Production).filter(Production.id == production_id).first()
+    if not production:
+        raise ValueError(f"Production with id '{production_id}' not found.")
     
+    events = db.query(Event).filter(Event.production_id == production_id).all()
+    return [f"{base_url}/events/{event.id}" for event in events]
 
-# /GET /productions/{production_id}/infos/{language_id}.
-# If we leave language ID as it is, we can have multiple prodinfos in the same language for one production.
-def get_production_info_id_service(db: Session, production_id: int, language_id: int, base_url: str) -> ProductionInfoResponse:
-    production_info = db.query(ProdInfo).filter(ProdInfo.production_id == production_id, ProdInfo.language_id == language_id).first()
-    if not production_info:
-        raise ValueError(f"Production info with production id '{production_id}' and language id '{language_id}' not found.")
-    return build_production_info_response(production_info, base_url)
-
-# /GET /productions/{production_id}.
-# Returns a copy of the production.
+# Returns a production with given id.
 def get_production_id_service(db: Session, production_id: int, base_url: str) -> ProductionResponse:
     production = db.query(Production).filter(Production.id == production_id).first()
     if not production:
-        raise ValueError(f"Production info with production id '{production_id}' not found.")
+        raise ValueError(f"Production with production id '{production_id}' not found.")
     return build_production_response(db, production, base_url)
 
-# /DELETE /productions/{production_id}/infos/{language_id} --> Deletes the production info for the given language.
-# Returns succes or failure.
-def delete_production_info_service(db: Session, production_id: int, language_id: int) -> bool:
-    production_info = db.query(ProdInfo).filter(ProdInfo.production_id == production_id, ProdInfo.language_id == language_id).first()
-    if not production_info:
-        return False
-
-    db.delete(production_info)
-    db.commit()
-    return True
-
-# /DELETE /productions/{production_id} --> Deletes the production and all related production infos.
-# Returns succes or failure. 
-def delete_production_service(db: Session, production_id: int) -> bool:
-    production = db.query(Production).filter(Production.id == production_id).first()
-    if not production:
-        return False
-
-    db.query(ProdInfo).filter(ProdInfo.production_id == production_id).delete()
-    db.delete(production)
-    db.commit()
-    return True
-
-def get_language_id_service(db: Session, language: str) -> int:
-    language = db.query(Language).filter(Language.name == language).first()
-    return language.id if language else None
-
-# /POST /productions/{production_id}/infos/{language_id} --> Creates a new production info for the given language.
-# Returns a copy of the created production info if not first info, else returns the production object.
-def create_production_info_service(db: Session, production_id: int, production_info_in: ProductionInfoCreate, language: str, base_url: str, first_info=False) -> ProductionInfoResponse:
+def create_production_info_service(db: Session, production_info_in: ProductionInfoCreate, production_id: int) -> ProdInfo:
     # Given language when creating new production info should exist in the database.
-    language_id = get_language_id_service(db, language)
-    if language_id is None:
-        raise ValueError(f"Language '{language}' not found in the database.")
+    language = db.query(Language).filter(Language.id == production_info_in.language_id).first()
+    if not language:
+        raise ValueError(f"Language with id '{production_info_in.language_id}' not found.")
 
     db_production_info = ProdInfo(
+        production_id=production_id,
+        language_id=production_info_in.language_id,
+
         title=production_info_in.title,
         supertitle=production_info_in.supertitle,
         artist=production_info_in.artist,
@@ -113,26 +103,17 @@ def create_production_info_service(db: Session, production_id: int, production_i
         teaser=production_info_in.teaser,
         description=production_info_in.description,
         info=production_info_in.info,
-        production_id=production_id,
-        language_id=language_id
     )
-
-    if not first_info:
-        db.add(db_production_info)
-        db.commit()
-        db.refresh(db_production_info)
-        return build_production_info_response(db_production_info, base_url)
-
     return db_production_info
 
-# /POST /productions --> Creates a new production and a new production info for the given language. 
+# Creates a new production with production info for the given language. 
 # Returns a copy of the created production.
-def create_production_service(db: Session, production_in: ProductionCreate, production_info_in: ProductionInfoCreate, language: str, base_url: str) -> ProductionResponse:
-    # Given language when creating new production should exist in the database.
-    language_id = get_language_id_service(db, language)
-    if language_id is None:
-        raise ValueError(f"Language '{language}' not found")
-    
+def create_production_service(db: Session, production_in: ProductionCreate, production_info_in: ProductionInfoCreate, language_id: int, base_url: str) -> ProductionResponse:
+    # Given language_id when creating new production should exist in the database.
+    language = db.query(Language).filter(Language.id == language_id).first()
+    if not language:
+        raise ValueError(f"Language with id '{language_id}' not found.")
+
     db_production = Production(
         performer_type=production_in.performer_type,
         attendance_mode=production_in.attendance_mode,
@@ -144,36 +125,52 @@ def create_production_service(db: Session, production_in: ProductionCreate, prod
     db.add(db_production)
     db.flush()
     
-    db_production_info = create_production_info_service(db, db_production.id, production_info_in, language, base_url, first_info=True)
+    db_production_info = create_production_info_service(db, production_info_in, db_production.id)
     
     db.add(db_production_info)
     db.commit()
     db.refresh(db_production)
-    db.refresh(db_production_info)
+    #db.refresh(db_production_info) --> needed?
     return build_production_response(db, db_production, base_url=base_url)
-
-# PATCH /productions/{production_id}/infos/{language_id} --> Updates the production info for the given language.
-def update_production_info_service(db: Session, production_id: int, language_id: int, production_info_in: ProductionInfoCreate, base_url: str) -> ProductionInfoResponse:
-    production_info = db.query(ProdInfo).filter(ProdInfo.production_id == production_id, ProdInfo.language_id == language_id).first()
-    if not production_info:
-        raise ValueError(f"Production info with production id '{production_id}' and language id '{language_id}' not found.")
-
-    for field, value in production_info_in.dict(exclude_unset=True).items():
-        setattr(production_info, field, value)
-
-    db.commit()
-    db.refresh(production_info)
-    return build_production_info_response(production_info, base_url)
-
-# Patch /productions/{production_id} --> Updates the production and all related production infos.
-def update_production_service(db: Session, production_id: int, production_in: ProductionCreate, base_url: str) -> ProductionResponse:
+    
+# Updates the production and all related production infos.
+def update_production_service(db: Session, production_in: ProductionUpdate, production_id: int, base_url: str = "") -> ProductionResponse:
     production = db.query(Production).filter(Production.id == production_id).first()
     if not production:
         raise ValueError(f"Production with id '{production_id}' not found.")
 
-    for field, value in production_in.dict(exclude_unset=True).items():
+    for field, value in production_in.model_dump(exclude_unset=True, exclude={"production_infos"}).items():
         setattr(production, field, value)
 
+    # Check if info is provided.
+    if production_in.production_infos:
+        existing = {info.language_id: info for info in production.info}
+        for production_info_in in production_in.production_infos:
+            if production_info_in.action == UpdateAction.DELETE:
+                if production_info_in.language_id not in existing:
+                    raise ValueError(f"Production info with production id '{production_id}' and language id '{production_info_in.language_id}' not found for deletion.")
+                db.delete(existing[production_info_in.language_id])  
+            else: # UPSERT
+                if production_info_in.language_id in existing:
+                    for field, value in production_info_in.model_dump(exclude_unset=True, exclude={"language_id", "action"}).items():
+                        setattr(existing[production_info_in.language_id], field, value)
+                else:
+                    db_production_info = create_production_info_service(db, production_info_in, production_id)
+                    db.add(db_production_info)
+            
     db.commit()
+    # Refreshes the whole production with eager loading (simplest).
     db.refresh(production)
     return build_production_response(db, production, base_url)
+
+# Deletes the production and all related production infos/events and returns success or failure.
+def delete_production_service(db: Session, production_id: int) -> bool:
+    production = db.query(Production).filter(Production.id == production_id).first()
+    if not production:
+        return False
+
+    db.query(ProdInfo).filter(ProdInfo.production_id == production_id).delete()
+    db.query(Event).filter(Event.production_id == production_id).delete()
+    db.delete(production)
+    db.commit()
+    return True
