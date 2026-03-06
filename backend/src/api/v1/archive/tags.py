5baf235603b13c763a47c4b620d24518d882675f
elerhels <elias.verhelst@ugent.be>
2026-03-06 12:33:19 +0100

    Backend: Add docs to /tag endpoints

--- backend/src/api/v1/archive/tags.py
+++ backend/src/api/v1/archive/tags.py
@@ -1,3 +1,4 @@
+from typing import List
 from fastapi import APIRouter, Depends, Request
 from sqlalchemy.orm import Session
 
@@ -7,22 +8,39 @@ from src.services.tag import create_tag, delete_tag_by_id, get_tag_by_id, get_ta
 
 router = APIRouter()
 
-@router.get("/")
+@router.get(
+    "/",
+    response_model=List[str],
+    summary="Get all tags",
+    description="Returns a list of tags."
+)
 async def get_tags(db: Session=Depends(get_db)):
     tags = get_tags_list(db)
     return tags
 
-@router.get("/{tag_id}", response_model=TagResponse)
+@router.get(
+    "/{tag_id}", 
+    response_model=TagResponse,
+    summary="Get a tag",
+    description="Get a tag and its corresponding names by id"
+)
 def get_tag(tag_id: int, request: Request, db: Session=Depends(get_db)):
     base_url = str(request.base_url).rstrip("/")
     tag = get_tag_by_id(db, tag_id, base_url)
     return TagResponse.model_validate(tag)
 
-@router.post("/", response_model=TagResponse)
+@router.post(
+    "/", 
+    response_model=TagResponse,
+    summary="Create a new tag",
+)
 async def post_tag(tag_in: TagCreate, request: Request, db: Session=Depends(get_db)):
     base_url = str(request.base_url).rstrip("/")
     return create_tag(db, tag_in, base_url)
 
-@router.delete("/{tag_id}")
+@router.delete(
+    "/{tag_id}",
+    summary="Delete a tag"
+)
 async def delete_tag(tag_id: str, db: Session=Depends(get_db)):
     return delete_tag_by_id(db, tag_id)
