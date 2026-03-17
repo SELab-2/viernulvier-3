16c71981dd3668887469eaf23c7487e488f879f5
elerhels <elias.verhelst@ugent.be>
2026-03-17 10:03:13 +0100

    improve template service hooks

--- frontend/app/features/_template-feature/hooks/resourceHooks.ts
+++ frontend/app/features/_template-feature/hooks/resourceHooks.ts
@@ -1,29 +1,49 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { getResourceById, getResourceList } from "../services/resourceService";
 import type { IResource } from "../resource.types";
 
 export function useGetResources(limit: number) {
   const [data, setData] = useState<IResource[] | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
+  const [error, setError] = useState<Error | null>(null);
 
-  getResourceList(20)
-    .then((data) => setData(data))
-    .catch(setError)
-    .finally(() => setLoading(false));
+  // Refresh function used to fetch and refetch data if needed
+  const refresh = useCallback(() => {
+    setLoading(true);
+    setError(null);
 
-  return { data, loading, error };
+    getResourceList(limit)
+      .then(setData)
+      .catch(setError)
+      .finally(() => setLoading(false));
+  }, [limit]);
+
+  useEffect(() => {
+    refresh();
+  }, [refresh]);
+
+  return { data, loading, error, refresh };
 }
 
 export function useGetResourceById(id: number) {
   const [data, setData] = useState<IResource | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
+  const [error, setError] = useState<Error | null>(null);
 
-  getResourceById(id)
-    .then((data) => setData(data))
-    .catch(setError)
-    .finally(() => setLoading(false));
+  // Refresh function used to fetch and refetch data if needed
+  const refresh = useCallback(() => {
+    setLoading(true);
+    setError(null);
 
-  return { data, loading, error };
+    getResourceById(id)
+      .then(setData)
+      .catch(setError)
+      .finally(() => setLoading(false));
+  }, [id]);
+
+  useEffect(() => {
+    refresh();
+  }, [refresh]);
+
+  return { data, loading, error, refresh };
 }
