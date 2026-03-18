5aca5469f156e5314e50daee164fdc25c9027413
KiandroDW <kiandro.dewaegenaere@ugent.be>
2026-03-18 10:18:23 +0100

    safely add halls

--- backend/src/worker/csv_wrapper.py
+++ backend/src/worker/csv_wrapper.py
@@ -2,6 +2,7 @@ import csv
 import logging
 
 from src.models.language import Language
+from src.models.hall import Hall
 from src.worker.converters.production import csv_prod_to_model_prod
 from src.worker.converters.event import csv_event_to_model_event
 from src.database import SESSION_LOCAL
@@ -45,6 +46,12 @@ with open(event_file, newline="", encoding="utf-8") as f:
 db = SESSION_LOCAL()
 logger.info("Connection with database created")
 
+# Get all halls
+hall_models = db.query(Hall).all()
+hall_map = {}
+for hall in hall_models:
+    hall_map[hall.name] = hall.id
+
 try:
     nl_lang_id = None
     languages = db.query(Language).all()
@@ -57,7 +64,14 @@ try:
         db.flush()
         if prod_id in events:
             for event in events[prod_id]:
-                event_model = csv_event_to_model_event(merged_production.id, event)
+                if event[2] in hall_map:
+                    hall_id = hall_map[event[2]]
+                else:
+                    hall_model = Hall(name=event[2])
+                    merged_hall = db.merge(hall_model)
+                    db.flush()
+                    hall_id = merged_hall.id
+                event_model = csv_event_to_model_event(merged_production.id, event, hall_id)
                 db.merge(event_model)
         db.commit()
 finally:
