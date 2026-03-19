02614aa1bc90a7bd64d1ccbba501063903aa6319
Stijn Van Biesen <Stijn.VanBiesen@UGent.be>
2026-03-19 15:50:09 +0100

    Created Navbar component

---
+++ frontend/app/routes/archive.tsx
@@ -0,0 +1,11 @@
+import Navbar from "~/shared/components/Navbar"
+
+export default function Archive() {
+  return (
+    <div>
+      <title>Archief | VIERNULVIER</title>
+      
+      <h1 className="text-3xl font-bold">Archief Pagina</h1>
+    </div>
+  );
+}
