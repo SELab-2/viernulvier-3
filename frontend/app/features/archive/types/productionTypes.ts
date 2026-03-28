9c9fe754c70d7126eb0bacdbdadb3ebfcbc5acd6
Stijn Van Biesen <Stijn.VanBiesen@UGent.be>
2026-03-28 16:13:52 +0100

    Finished productionTypes.ts

--- frontend/app/features/archive/types/productionTypes.ts
+++ frontend/app/features/archive/types/productionTypes.ts
@@ -1,35 +1,79 @@
+export interface Pagination {
+  next_cursor?: number;
+  has_more: boolean;
+}
+
 export interface ProductionInfo {
   id_url: string;
   language: string;
-  title: string;
-  supertitle: string;
-  artist: string;
-  tagline: string;
-  teaser: string;
-  description: string;
-  info: string;
+  
+  title?: string;
+  supertitle?: string;
+  artist?: string;
+  tagline?: string;
+  teaser?: string;
+  description?: string;
+  info?: string;
 }
 
 export interface Production {
   id_url: string;
-  performer_type: string;
-  attendance_mode: string;
-  media_gallery_id: any; // Update after merge media pr.
-  created_at: string;
-  updated_at: string;
+  
+  performer_type?: string;
+  attendance_mode?: string;
+  media_gallery_id?: number; // Maybe update after merge media pr.
+  created_at?: string;
+  updated_at?: string;
+  
+  production_infos: ProductionInfo[];
+  events: string[];
+  tags: string[]; // ToDo: change after pr.
 }
 
-export interface EventCreate {
-  production_id: string;
-  hall_id: string;
-  starts_at?: string;
-  ends_at?: string;
-  order_url?: string;
+export interface ProductionList {
+  productions: Production[];
+  pagination: Pagination;
 }
 
-export interface EventUpdate {
-  hall_id?: string;
-  starts_at?: string;
-  ends_at?: string;
-  order_url?: string;
+export interface ProductionInfoCreate {
+  language: string;
+
+  title?: string;
+  supertitle?: string;
+  artist?: string; 
+  tagline?: string;
+  teaser?: string;
+  description?: string;
+  info?: string;
+}
+
+export interface ProductionCreate {
+  performer_type?: string;
+  attendance_mode?: string;
+  media_gallery_id?: number; // Maybe update after merge media pr.
+
+  production_info: ProductionInfoCreate;
+  tag_ids: number[];
+}
+
+export interface ProductionInfoUpdate {
+  language: string;
+
+  title?: string;
+  supertitle?: string;
+  artist?: string;
+  tagline?: string;
+  teaser?: string;
+  description?: string;
+  info?: string; 
+}
+
+export interface ProductionUpdate {
+  performer_type?: string;
+  attendance_mode?: string; 
+  media_gallery_id?: number; // Maybe update after merge media pr.
+
+  production_infos?: ProductionInfoUpdate[];
+  tag_ids?: number[];
+  remove_languages?: string[]; 
 }
