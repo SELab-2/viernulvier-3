883aaf33d83d1ba5fa0e399bd733c1470611e144
elerhels <elias.verhelst@ugent.be>
2026-03-29 15:52:32 +0200

    Create tag service

---
+++ frontend/app/features/archive/services/tagService.ts
@@ -0,0 +1,30 @@
+import {
+  getFromArchive,
+  getFromArchiveList,
+  postToArchive,
+  patchToArchive,
+  deleteFromArchive,
+} from "~/shared/services/sharedService";
+import type { TagCreate, Tag, TagUpdate } from "../types/tagTypes";
+
+const ENDPOINT = "/tags";
+
+export async function getTagList(): Promise<Tag[]> {
+  return getFromArchiveList<Tag>(ENDPOINT);
+}
+
+export async function getTagById(id: number): Promise<Tag> {
+  return getFromArchive<Tag>(`${ENDPOINT}/${id}`);
+}
+
+export async function createTag(request: TagCreate): Promise<Tag> {
+  return postToArchive<Tag>(`${ENDPOINT}`, request);
+}
+
+export async function editTag(id: number, request: TagUpdate): Promise<Tag> {
+  return patchToArchive<Tag>(`${ENDPOINT}/${id}`, request);
+}
+
+export async function deleteTag(id: number): Promise<void> {
+  return deleteFromArchive(`${ENDPOINT}/${id}`);
+}
