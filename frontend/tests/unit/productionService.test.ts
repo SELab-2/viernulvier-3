55837010b314a1af0a4c3f691cc74c5fae862887
Stijn Van Biesen <Stijn.VanBiesen@UGent.be>
2026-04-01 21:36:44 +0200

    Started on tests

--- frontend/tests/unit/productionService.test.ts
+++ frontend/tests/unit/productionService.test.ts
@@ -13,6 +13,7 @@ import {
 } from "~/features/archive/services/productionService";
 
 import type { Event } from "~/features/archive/types/eventTypes";
+import type { Tag, TagName } from "~/features/archive/types/tagTypes";
 
 import type {
   Production,
@@ -28,6 +29,36 @@ setupLocalStorage();
 
 describe("productionService", () => {
   let mockAdapter: AxiosMockAdapter;
+
+  const tagname_en: TagName = {
+    language: "en",
+    name: "tag_en"
+  }
+
+  const tagname_nl: TagName = {
+    language: "nl",
+    name: "tag_nl"
+  }
+
+  const tag1: Tag = {
+    id: "/api/v1/archive/tags/1",
+    names: [tagname_en, tagname_nl]
+  }
+
+  const tag2: Tag = {
+    id: "/api/v1/archive/tags/2",
+    names: [tagname_en, tagname_nl]
+  }
+
+  const tag3: Tag = {
+    id: "/api/v1/archive/tags/3",
+    names: [tagname_en, tagname_nl]
+  }
+
+  const tag4: Tag = {
+    id: "/api/v1/archive/tags/4",
+    names: [tagname_en, tagname_nl]
+  }
 
   const mockEvent1: Event = {
     id: "/api/v1/archive/events/1",
@@ -81,7 +112,7 @@ describe("productionService", () => {
     
     production_infos: [mockProductionInfo1NL, mockProductionInfo1EN],
     events: ["/api/v1/archive/events/1", "/api/v1/archive/events/2"],
-    tags: ["tag1, tag2"]
+    tags: [tag1, tag2]
   };
   const mockProduction2: Production = {
     id: "/api/v1/archive/productions/2",
@@ -92,7 +123,7 @@ describe("productionService", () => {
     
     production_infos: [mockProductionInfo2NL],
     events: ["/api/v1/archive/events/1", "/api/v1/archive/events/2"],
-    tags: ["tag3"]
+    tags: [tag3, tag4]
   };
 
   beforeEach(() => {
@@ -106,12 +137,68 @@ describe("productionService", () => {
     mockAdapter.onGet("/api/v1/archive/events/2").reply(200, mockEvent2);
     mockAdapter.onGet("/api/v1/archive/productions/1").reply(200, mockProduction1);
     mockAdapter.onGet("/api/v1/archive/productions/2").reply(200, mockProduction2);
+    mockAdapter.onGet("/api/v1/archive/tags/1").reply(200, tag1);
+    mockAdapter.onGet("/api/v1/archive/tags/2").reply(200, tag2);
+    mockAdapter.onGet("/api/v1/archive/tags/3").reply(200, tag3);
+    mockAdapter.onGet("/api/v1/archive/tags/4").reply(200, tag4);
     vi.spyOn(axios, "create").mockReturnValue(apiClient);
   });
 
   describe("getProduction", () => {
     it("returns a single production by id on success", async () => {
-      
+      const result = await getProduction(1);
+
+      expect(result).toBeDefined();
+
+      expect(result.id).toBe(mockProduction1.id);
+      expect(result.performer_type).toBe(mockProduction1.performer_type);
+      expect(result.attendance_mode).toBe(mockProduction1.attendance_mode);
+
+      expect(result.production_infos).toHaveLength(2);
+      expect(result.production_infos).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({ language: "nl" }),
+          expect.objectContaining({ language: "en" }),
+        ])
+      );
+
+      expect(result.events).toHaveLength(2);
+      expect(result.events).toEqual(mockProduction1.events);
     });
-  })
+  });
+  describe("createProduction", () => {
+    it("creates a new production and returns it", async () => {
+      const production_info: ProductionInfo = {
+        prod_id: "/api/v1/archive/productions/3",
+        language: "en",
+        title: "Rocking the Suburbs!",
+        artist: "William Shatner"
+      }
+      const productionData: ProductionCreate = {
+        performer_type: "singer",
+        attendance_mode: "online",
+        production_info: production_info,
+        tag_ids: [1, 2],
+      }
+
+      const mockResponse: Production = {
+        id: "/api/v1/archive/productions/3",
+        production_infos: [production_info],
+        events: [],
+        tags: [tag1]
+      }
+
+      mockAdapter.onPost("/api/v1/archive/productions").reply(201, mockResponse);
+      const result = await createProduction(productionData);
+      expect(result).toEqual(mockResponse);
+      expect(result.id).toBe("/api/v1/archive/productions/3");
+
+    });
+  });
+  describe("updateProduction", () => {
+    it("updates a production and returns updated data", async () => {});
+  });
+  describe("deleteProduction", () => {
+    it("deletes an event successfully", async () => {});
+  });
 })
