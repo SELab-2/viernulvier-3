b3c49305e652bdba63eb39091b4d2ab018d542de
Stijn Van Biesen <Stijn.VanBiesen@UGent.be>
2026-04-01 22:38:28 +0200

    Finalize + clean-up

--- frontend/tests/unit/productionService.test.ts
+++ frontend/tests/unit/productionService.test.ts
@@ -12,15 +12,15 @@ import {
   deleteProduction,
 } from "~/features/archive/services/productionService";
 
-import type { Event } from "~/features/archive/types/eventTypes";
-import type { Tag, TagName } from "~/features/archive/types/tagTypes";
+import type { Tag } from "~/features/archive/types/tagTypes";
 
 import type {
   Production,
   ProductionInfo,
   ProductionList,
   ProductionCreate,
-  ProductionUpdate
+  ProductionUpdate,
+  PaginationResponse
 } from "~/features/archive/types/productionTypes";
 
 import { setupLocalStorage } from "tests/globalSetup";
@@ -30,58 +30,18 @@ setupLocalStorage();
 describe("productionService", () => {
   let mockAdapter: AxiosMockAdapter;
 
-  const tagname_en: TagName = {
-    language: "en",
-    name: "tag_en"
-  }
-
-  const tagname_nl: TagName = {
-    language: "nl",
-    name: "tag_nl"
-  }
-
+  // Mock tags
   const tag1: Tag = {
     id: "/api/v1/archive/tags/1",
-    names: [tagname_en, tagname_nl]
+    names: []
   }
 
   const tag2: Tag = {
     id: "/api/v1/archive/tags/2",
-    names: [tagname_en, tagname_nl]
+    names: []
   }
 
-  const tag3: Tag = {
-    id: "/api/v1/archive/tags/3",
-    names: [tagname_en, tagname_nl]
-  }
-
-  const tag4: Tag = {
-    id: "/api/v1/archive/tags/4",
-    names: [tagname_en, tagname_nl]
-  }
-
-  const mockEvent1: Event = {
-    id: "/api/v1/archive/events/1",
-    production_id: "/api/v1/archive/productions/1",
-    hall_id: "hall1",
-    starts_at: "2026-04-29T19:00:00",
-    ends_at: "2026-04-29T22:00:00",
-    order_url: "https://example.com/order",
-    price_ids: ["price1", "price2"],
-    created_at: "2026-03-20T10:00:00",
-    updated_at: "2026-03-20T10:00:00",
-  };
-  const mockEvent2: Event = {
-    id: "/api/v1/archive/events/2",
-    production_id: "/api/v1/archive/productions/1",
-    hall_id: "hall3",
-    starts_at: "2026-04-29T19:00:00",
-    ends_at: "2026-04-29T22:00:00",
-    order_url: "https://example.com/order",
-    price_ids: ["price1", "price2"],
-    created_at: "2026-03-29T14:00:00",
-    updated_at: "2026-03-29T14:00:00",
-  };
+  // Mock productions (+ info)
   const mockProductionInfo1NL: ProductionInfo = {
     prod_id: "/api/v1/archive/productions/1",
     language: "nl",
@@ -89,6 +49,7 @@ describe("productionService", () => {
     artist: "Homer Simpson",
     description: "Een kijkje achter de schermen van de wereldbenoemde serie: De Simpsons, rechtstreeks uit de ogen van Homer Simpson!",
   };
+
   const mockProductionInfo1EN: ProductionInfo = {
     prod_id: "/api/v1/archive/productions/1",
     language: "en",
@@ -96,6 +57,7 @@ describe("productionService", () => {
     artist: "Homer Simpson",
     description: "A peek behind the scenes of the world-famous series: The Simpsons, straight from Homer Simpson’s perspective!",
   };
+
   const mockProductionInfo2NL: ProductionInfo = {
     prod_id: "/api/v1/archive/productions/2",
     language: "nl",
@@ -103,6 +65,7 @@ describe("productionService", () => {
     artist: "Jokke",
     description: "Jokke neeemt jullie mee naar het verleden met zijn oude nummers.",
   };
+
   const mockProduction1: Production = {
     id: "/api/v1/archive/productions/1",
     performer_type: "documentary",
@@ -114,6 +77,7 @@ describe("productionService", () => {
     events: ["/api/v1/archive/events/1", "/api/v1/archive/events/2"],
     tags: [tag1, tag2]
   };
+
   const mockProduction2: Production = {
     id: "/api/v1/archive/productions/2",
     performer_type: "singer",
@@ -123,7 +87,7 @@ describe("productionService", () => {
     
     production_infos: [mockProductionInfo2NL],
     events: ["/api/v1/archive/events/1", "/api/v1/archive/events/2"],
-    tags: [tag3, tag4]
+    tags: [tag2]
   };
 
   beforeEach(() => {
@@ -133,15 +97,28 @@ describe("productionService", () => {
 
     const apiClient = createApiClient();
     mockAdapter = new AxiosMockAdapter(apiClient);
-    mockAdapter.onGet("/api/v1/archive/events/1").reply(200, mockEvent1);
-    mockAdapter.onGet("/api/v1/archive/events/2").reply(200, mockEvent2);
     mockAdapter.onGet("/api/v1/archive/productions/1").reply(200, mockProduction1);
     mockAdapter.onGet("/api/v1/archive/productions/2").reply(200, mockProduction2);
-    mockAdapter.onGet("/api/v1/archive/tags/1").reply(200, tag1);
-    mockAdapter.onGet("/api/v1/archive/tags/2").reply(200, tag2);
-    mockAdapter.onGet("/api/v1/archive/tags/3").reply(200, tag3);
-    mockAdapter.onGet("/api/v1/archive/tags/4").reply(200, tag4);
     vi.spyOn(axios, "create").mockReturnValue(apiClient);
+  });
+
+  describe("getProductions", () => {
+    it("returns productions list", async () => {
+      const pagination: PaginationResponse = {
+        has_more: false,
+      } 
+      
+      const mockProductionList: ProductionList = {
+        productions: [mockProduction1, mockProduction2],
+        pagination: pagination
+      }
+
+      mockAdapter.onGet("/api/v1/archive/productions").reply(200, mockProductionList);
+      const result = await getProductionsPaginated();
+
+      expect(result.productions).toHaveLength(2);
+      expect(result.pagination.has_more).toBe(false);
+    });
   });
 
   describe("getProduction", () => {
@@ -166,6 +143,7 @@ describe("productionService", () => {
       expect(result.events).toEqual(mockProduction1.events);
     });
   });
+
   describe("createProduction", () => {
     it("creates a new production and returns it", async () => {
       const production_info: ProductionInfo = {
@@ -192,13 +170,74 @@ describe("productionService", () => {
       const result = await createProduction(productionData);
       expect(result).toEqual(mockResponse);
       expect(result.id).toBe("/api/v1/archive/productions/3");
+    });
 
+    it("throws when create request fails", async () => {
+      const production_info: ProductionInfo = {
+        prod_id: "/api/v1/archive/productions/3",
+        language: "en",
+        title: "Rocking the Suburbs!",
+        artist: "William Shatner"
+      }
+      const productionData: ProductionCreate = {
+        performer_type: "singer",
+        attendance_mode: "online",
+        production_info: production_info,
+        tag_ids: [1, 2],
+      }
+
+      mockAdapter.onPost("/api/v1/archive/productions").reply(400);
+
+      await expect(createProduction(productionData)).rejects.toThrow();
     });
   });
   describe("updateProduction", () => {
-    it("updates a production and returns updated data", async () => {});
+    it("updates a production and returns updated data", async () => {
+      const productionUpdate: ProductionUpdate = {
+        attendance_mode: "online"
+      }
+
+      const mockProduction2_updated: Production = {
+        id: "/api/v1/archive/productions/2",
+        performer_type: "singer",
+        attendance_mode: "online",
+        created_at: "2026-03-29T14:00:00",
+        updated_at: "2026-03-29T14:00:00",
+        
+        production_infos: [mockProductionInfo2NL],
+        events: ["/api/v1/archive/events/1", "/api/v1/archive/events/2"],
+        tags: [tag1]
+      };
+
+      mockAdapter.onPatch("/api/v1/archive/productions/2").reply(200, mockProduction2_updated);
+      
+      const result = await updateProduction(2, productionUpdate);
+
+      expect(result).toEqual(mockProduction2_updated);
+      expect(result.attendance_mode).toBe("online");
+      expect(result.tags[0]["id"]).toBe("/api/v1/archive/tags/1");
+    });
+
+    it("throws when update request fails", async () => {
+      const productionUpdate: ProductionUpdate = {
+        attendance_mode: "online"
+      }
+
+      mockAdapter.onPatch("/api/v1/archive/productions/2").reply(400);
+      await expect(updateProduction(1, productionUpdate)).rejects.toThrow();
+    });
   });
   describe("deleteProduction", () => {
-    it("deletes an event successfully", async () => {});
+    it("deletes a production successfully", async () => {
+      mockAdapter.onDelete("/api/v1/archive/productions/1").reply(204);
+
+      await expect(deleteProduction(1)).resolves.toBeUndefined();
+    });
+
+    it("throws when delete request fails", async () => {
+      mockAdapter.onDelete("/api/v1/archive/productions/1").reply(404);
+
+      await expect(deleteProduction(1)).rejects.toThrow();
+    });
   });
-})
+});
