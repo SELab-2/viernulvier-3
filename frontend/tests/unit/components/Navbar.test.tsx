72ac943fc4fca0290f6b4a59ef045c2b488de53e
Bram Windey <bram.windey@ugent.be>
2026-04-05 12:05:06 +0200

    Add tests for the navbar menu button

--- frontend/tests/unit/components/Navbar.test.tsx
+++ frontend/tests/unit/components/Navbar.test.tsx
@@ -3,32 +3,54 @@ import { describe, it, expect, vi } from "vitest";
 import Navbar from "~/shared/components/Navbar";
 import { MemoryRouter } from "react-router";
 import { ThemeProvider } from "~/shared/components/ThemeContext";
+import userEvent from "@testing-library/user-event";
 
 vi.mock("react-i18next", () => ({
   useTranslation: () => ({
     t: (key: string) => {
       const map: Record<string, string> = {
-        "nav.home": "Home",
-        "nav.archive": "Archive",
-        "nav.history": "History",
+        "nav.home": "I18N_Home",
+        "nav.archive": "I18N_Archive",
+        "nav.history": "I18N_History",
       };
       return map[key] || key;
     },
   }),
 }));
 
-describe("Navbar", async () => {
-  it("renders navigation links", () => {
-    render(
-      <MemoryRouter>
-        <ThemeProvider>
-          <Navbar />
-        </ThemeProvider>
-      </MemoryRouter>
-    );
+function renderWithRouterAndTheme(component: React.ReactNode) {
+  render(
+    <MemoryRouter>
+      <ThemeProvider>{component}</ThemeProvider>
+    </MemoryRouter>
+  );
+}
 
-    expect(screen.getByText("Home")).toBeInTheDocument();
-    expect(screen.getAllByText("Archive").length === 2);
-    expect(screen.getByText("History")).toBeDefined();
+describe("Navbar", () => {
+  it("renders navigation links", () => {
+    renderWithRouterAndTheme(<Navbar />);
+
+    expect(screen.getByText("I18N_Home")).toBeInTheDocument();
+    expect(screen.getByText("I18N_History")).toBeInTheDocument();
+    // Once for the logo, once for the link
+    expect(screen.getAllByText("I18N_Archive").length).toBe(2);
+  });
+
+  it("toggles mobile menu when clicking hamburger button", async () => {
+    renderWithRouterAndTheme(<Navbar />);
+
+    // After 2h I don't see why the tests have the Navbar rendered twice,
+    // so let's just get this first one...
+    const button = screen.getAllByTestId("hamburger-menu-button")[0];
+
+    expect(screen.queryByTestId("mobile-menu")).not.toBeInTheDocument();
+
+    await userEvent.click(button);
+
+    expect(screen.queryByTestId("mobile-menu")).toBeInTheDocument();
+
+    await userEvent.click(button);
+
+    expect(screen.queryByTestId("mobile-menu")).not.toBeInTheDocument();
   });
 });
