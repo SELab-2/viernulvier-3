e1366b660a1714d595634f78d4f57247d0ea51f5
Bram Windey <bram.windey@ugent.be>
2026-04-12 20:25:14 +0200

    Fix production service tags

--- backend/src/services/production.py
+++ backend/src/services/production.py
@@ -176,7 +176,9 @@ def create_production(
             f"Language '{production_info_in.language}' not supported."
         )
 
-    tag_ids = production_in.tag_ids or []
+    tag_id_urls = production_in.tag_id_urls or []
+    tag_ids = [int(tag_url.rstrip("/").split("/")[-1]) for tag_url in tag_id_urls]
+
     existing_tags = db.query(Tag).filter(Tag.id.in_(tag_ids)).all()
     existing_tag_ids = {t.id for t in existing_tags}
     missing_tag_ids = set(tag_ids) - existing_tag_ids
@@ -221,7 +223,7 @@ def update_production_by_id(
     # Check for tags.
     if production_in.tag_id_urls is not None:
         tag_id_urls = production_in.tag_id_urls or []
-        tag_ids = [id_url.rstrip("/").split("/")[-1] for id_url in tag_id_urls]
+        tag_ids = [int(id_url.rstrip("/").split("/")[-1]) for id_url in tag_id_urls]
         existing_tags = db.query(Tag).filter(Tag.id.in_(tag_ids)).all()
         existing_tag_ids = {t.id for t in existing_tags}
         missing_tag_ids = set(tag_ids) - existing_tag_ids
