e04af5b1c296f2a8ca6c9c1ad2ae4ef108f81cc1
Stijn Van Biesen <Stijn.VanBiesen@UGent.be>
2026-04-15 10:53:04 +0200

    Add translations to 404

--- frontend/app/routes/not-found.tsx
+++ frontend/app/routes/not-found.tsx
@@ -9,14 +9,14 @@ export default function NotFound() {
       <title>{`${t("nav.home")} | VIERNULVIER`}</title>
       <div className="mx-auto max-w-5xl py-12 text-center md:py-24">
         <h1 className="mb-6 font-serif text-5xl italic md:mb-12 md:text-8xl">
-          {"VIERNULVIER - " + t("notFound.title")}
+          {`VIERNULVIER - ${t("notFound.title")}`}
         </h1>
         <p className="mb-6 text-lg md:mb-12 md:text-2xl">{t("notFound.description")}</p>
         <div className="flex flex-col justify-center space-y-4 sm:flex-row sm:space-y-0 sm:space-x-8">
-          <NavigateButton location="archive" name={t("home.buttons.explore")} />
+          <NavigateButton location="archive" name={t("notFound.buttons.explore")} />
           <NavigateButton
             location="history"
-            name={t("home.buttons.history")}
+            name={t("notFound.buttons.history")}
             variant="secondary"
           />
         </div>
