9558326d9c098da69e88a8b90c62d102c4e07ced
elerhels <elias.verhelst@ugent.be>
2026-04-16 13:27:54 +0200

    Bump favicon version

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -15,6 +15,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta charSet="utf-8" suppressHydrationWarning />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <script dangerouslySetInnerHTML={{ __html: getThemeBootstrapScript() }} />
+        <link rel="icon" href={`/favicon.ico?v=2`} />
         <Meta />
         <Links />
       </head>
