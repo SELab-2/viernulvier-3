9314f82be18db9d4dead32216ac2ab2657786499
Nathan Vermeiren <nathan.vermeiren@ugent.be>
2026-04-17 15:19:53 +0200

    use StrictModel for StatisticsResponse
    Co-authored-by: BWindey <bram.windey@ugent.be>

--- backend/src/schemas/statistics.py
+++ backend/src/schemas/statistics.py
@@ -1,7 +1,7 @@
-from pydantic import BaseModel
+from src.schemas.base_schema import StrictModel
 
 
-class StatisticsResponse(BaseModel):
+class StatisticsResponse(StrictModel):
     productions_count: int = 0
     events_count: int = 0
     unique_artists_count: int = 0
