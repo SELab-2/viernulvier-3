652c08a96241bd2575d0a5dc00640777cdb7232b
Bram Windey <bram.windey@ugent.be>
2026-04-22 14:56:27 +0200

    Add 'updateProductionByUrl' service

--- frontend/app/features/archive/services/productionService.ts
+++ frontend/app/features/archive/services/productionService.ts
@@ -4,6 +4,7 @@ import {
   postToArchive,
   patchToArchive,
   deleteFromArchive,
+  patchByUrl,
 } from "~/shared/services/sharedService";
 import type {
   Production,
@@ -43,6 +44,13 @@ export async function createProduction(
   productionData: ProductionCreate
 ): Promise<Production> {
   return postToArchive<Production>("/productions", productionData);
+}
+
+export async function updateProductionByUrl(
+  production_url: string,
+  productionData: ProductionUpdate
+): Promise<Production> {
+  return patchByUrl<Production>(production_url, productionData);
 }
 
 export async function updateProduction(
