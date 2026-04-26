0d078fe2026bafe1a849d200f5b3d67f4f4a656e
nathan <nathan.vermeiren@ugent.be>
2026-04-26 17:15:26 +0200

    make new DB table for history

---
+++ backend/src/models/history.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, String, Text, UniqueConstraint
+from src.database import Base
+
+
+class History(Base):
+    __tablename__ = "history"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+
+    year = Column(Integer, nullable=False)
+
+    # 'nl', 'en'
+    language = Column(String(5), nullable=False)
+
+    title = Column(String(255))
+    content = Column(Text, nullable=False)
+
+    __table_args__ = (
+        UniqueConstraint("year", "language", name="uix_year_language"),
+    )
