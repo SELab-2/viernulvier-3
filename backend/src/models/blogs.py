300fd4f16ad3968dc2951e11b2d0dffbdcb1a5b4
KiandroDW <kiandro.dewaegenaere@ugent.be>
2026-04-27 16:13:01 +0200

    discussion 5 says authors are not required

--- backend/src/models/blogs.py
+++ backend/src/models/blogs.py
@@ -11,9 +11,6 @@ class Blog(Base):
     __tablename__ = "blogs"
     id = Column(Integer, primary_key=True, autoincrement=True)
 
-    author_id = Column(Integer, ForeignKey("users.id"))
-    author = relationship("User", back_populates="blogs")
-
     productions = relationship(
         "Production",
         secondary=prod_blogs,
