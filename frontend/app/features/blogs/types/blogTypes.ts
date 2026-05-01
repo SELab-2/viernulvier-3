9ba1392b914da18366f1a894892fbb2143e1c8d0
KiandroDW <kiandro.dewaegenaere@ugent.be>
2026-05-01 11:59:34 +0200

    blog types

---
+++ frontend/app/features/blogs/types/blogTypes.ts
@@ -0,0 +1,45 @@
+import type { PaginationResponse } from "~/features/archive/types/paginationTypes";
+
+export interface BlogContent {
+  blog_id_url: string;
+  language: string;
+
+  title: string;
+  content: string;
+}
+
+export interface Blog {
+  id_url: string;
+  blog_contents: BlogContent[];
+  production_id_urls: string[];
+}
+
+export interface BlogList {
+  blogs: Blog[];
+  pagination: PaginationResponse;
+}
+
+export interface BlogContentCreate {
+  language: string;
+
+  title: string;
+  content: string;
+}
+
+export interface BlogCreate {
+  blog_content: BlogContentCreate;
+  production_id_urls: string[];
+}
+
+export interface BlogContentUpdate {
+  language: string;
+
+  title: string;
+  content: string;
+}
+
+export interface BlogUpdate {
+  blog_contents: BlogContentUpdate[];
+  production_id_urls: string[];
+  remove_languages?: string[];
+}
