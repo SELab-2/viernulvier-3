abec0d6e9374a2817114d212bce7b44e84945f1f
KiandroDW <kiandro.dewaegenaere@ugent.be>
2026-05-02 15:53:00 +0200

    used blog services in blogs page

--- frontend/app/features/blogs/pages/BlogPage.tsx
+++ frontend/app/features/blogs/pages/BlogPage.tsx
@@ -1,5 +1,7 @@
 import { BlogCardList } from "../components/BlogCard";
+import { getBlogsPaginated } from "../services/blogService";
 
-export default function BlogPage() {
-  return <BlogCardList />;
+export default async function BlogPage() {
+  const blogs = await getBlogsPaginated();
+  return <BlogCardList blogs={blogs.blogs}/>;
 }
