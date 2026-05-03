4ac0914f93b239f80686e557bff12c587e51d385
KiandroDW <kiandro.dewaegenaere@ugent.be>
2026-05-03 10:09:19 +0200

    fixed issue with blogpage

--- frontend/app/features/blogs/pages/BlogPage.tsx
+++ frontend/app/features/blogs/pages/BlogPage.tsx
@@ -1,7 +1,22 @@
+import type { Blog } from "../types/blogTypes";
+import { useEffect, useState } from "react";
 import { BlogCardList } from "../components/BlogCard";
 import { getBlogsPaginated } from "../services/blogService";
 
-export default async function BlogPage() {
-  const blogs = await getBlogsPaginated();
-  return <BlogCardList blogs={blogs.blogs} />;
+export default function BlogPage() {
+  const [blogs, setBlogs] = useState<Blog[]>([]);
+
+  useEffect(() => {
+    async function fetchBlogs() {
+      const result = await getBlogsPaginated();
+      setBlogs(result.blogs);
+    }
+    fetchBlogs();
+  }, []);
+
+  return (
+    <div>
+      <BlogCardList blogs={blogs} />
+    </div>
+  );
 }
