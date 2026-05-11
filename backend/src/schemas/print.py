1898d0281dfb16907df36e26dd9947e16a018654
Alexander Vandeputte <alexander.vandeputte88@gmail.com>
2026-05-11 16:45:05 +0200

    added enum to schema

--- backend/src/schemas/print.py
+++ backend/src/schemas/print.py
@@ -1,9 +1,16 @@
 from datetime import datetime
+from enum import Enum
 from typing import Optional
 
 from src.schemas.base_schema import StrictModel
-
 from src.schemas.pagination import IdPagination
+
+
+class PrintType(str, Enum):
+    POSTER = "poster"
+    TIMETABLE = "timetable"
+    PROGRAMME = "programme"
+    OTHER = "other"
 
 
 class PrintResponse(StrictModel):
@@ -12,7 +19,7 @@ class PrintResponse(StrictModel):
     content_type: str
     title: Optional[str] = None
     description: Optional[str] = None
-    print_type: Optional[str] = None
+    print_type: Optional[PrintType] = None
     uploaded_at: datetime
 
     model_config = {"from_attributes": True}
