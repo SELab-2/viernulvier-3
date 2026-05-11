1b72535146fb3db0f7b1edfbe8c944e59c4ca950
Bram Windey <bram.windey@ugent.be>
2026-05-11 16:40:32 +0200

    Update sync_store so that it does not add duplicate viernulvier_id's

--- backend/src/worker/sync/store/genre.py
+++ backend/src/worker/sync/store/genre.py
@@ -2,6 +2,8 @@ import logging
 
 from datetime import datetime
 from sqlalchemy.orm import Session
+from sqlalchemy.sql import select
+from src.models.tag import Tag
 from src.worker.converters.genres import api_genre_to_model_tag
 
 logger = logging.getLogger(__name__)
@@ -10,9 +12,16 @@ logger = logging.getLogger(__name__)
 def store_new_genres(db_session: Session, genres: list[dict]):
     newest_timestamp = None
 
+    existing_vnv_ids = set(db_session.execute(select(Tag.viernulvier_id)))
+
     for json_genre in genres:
         try:
             tag = api_genre_to_model_tag(json_genre)
+
+            if tag.viernulvier_id in existing_vnv_ids:
+                continue
+            else:
+                existing_vnv_ids.add(tag.viernulvier_id)
 
             db_session.add(tag)
 
