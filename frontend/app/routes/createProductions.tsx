a0122a4321f36b3c6ee6b068e6421e1c3441e20e
Stijn Van Biesen <Stijn.VanBiesen@UGent.be>
2026-05-12 19:11:35 +0200

    fix name

--- frontend/app/routes/createProductions.tsx
+++ frontend/app/routes/createProductions.tsx
@@ -2,7 +2,7 @@ import { Protected } from "~/features/auth";
 import { ARCHIVE_PERMISSIONS } from "~/features/archive/archive.constants";
 import { CreateProductionPageAccessDenied, CreateProductionPage } from "~/features/archive/pages/CreateProductionPage";
 
-export default function CreateBlogRoute() {
+export default function CreateProductionRoute() {
   return (
     <Protected
       permissions={[ARCHIVE_PERMISSIONS.create]}
