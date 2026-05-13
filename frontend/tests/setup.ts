74d6341623ee529cfac56a33ce6243d1760599bc
Stijn Van Biesen <Stijn.VanBiesen@UGent.be>
2026-05-13 21:39:46 +0200

    add some tests

--- frontend/tests/setup.ts
+++ frontend/tests/setup.ts
@@ -29,6 +29,7 @@ const translationMap: Record<string, TranslationValue> = {
   "home.buttons.history": "I18N_Home_Button_History",
   "archive.title": "I18N_Archive_Title",
   "archive.create_production": "I18_Archive_Create_Production",
+  "archive.add_info.title": "I18_Archive_addInfo_Title",
   "archive.accessDenied.title": "I18N_Archive_Access_Denied_Title",
   "archive.accessDenied.description": "I18N_Archive_Access_Denied_Description",
   "history.title": "I18N_History_Title",
@@ -100,6 +101,8 @@ const translationMap: Record<string, TranslationValue> = {
   "productionPage.edit.title": "I18N_ProductionInfo_Edit_Title",
   "productionPage.edit.teaser": "I18N_ProductionInfo_Edit_Teaser",
   "productionPage.edit.description": "I18N_ProductionInfo_Edit_Description",
+  "productionPage.edit.save": "I18N_ProductionPage_Edit_Save",
+  "productionPage.edit.cancel": "I18N_ProductionPage_Edit_Cancel",  
 };
 
 const mockTranslate = (key: string, options?: TranslationOptions) => {
